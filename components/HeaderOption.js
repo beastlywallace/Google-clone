ca436c91b1de114dc2ce9c3f5745d1c2fbd6cf5d
beastlywallace <akonakpevwe@gmail.com>
2022-12-26 06:31:34 +0100

    headerOption

---
+++ components/HeaderOption.js
@@ -0,0 +1,7 @@
+'
+
+export const HeaderOption = () => {
+  return (
+    <div>HeaderOption</div>
+  )
+}
--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import {MapIcon,PlayIcon, DotsVerticalIcon, SearchIcon, PhotographIcon, NewspaperIcon} from '@heroicons/react/outline'
 export const HeaderOptions = () => {
   return (
-    <div>HeaderOptions</div>
+    <div>
+      <div className='flex space-x-6'>
+        <HeaderOption icon={SearchIcon }  title="All" selected/>
+        <HeaderOption icon={ PhotographIcon}  title="Images"/>
+        <HeaderOption icon={ PlayIcon}  title="Videos"/>
+        <HeaderOption icon={NewspaperIcon}  title="News"/>
+        <HeaderOption icon={MapIcon}  title="Maps"/>
+        <HeaderOption icon={DotsVerticalIcon }  title="More"/>
+      </div>
+      <div className='flex space-x-4'>
+        <p className='link'>settings</p>
+        <p className='link'>tools</p>
+      </div>
+    </div>
   )
 }
